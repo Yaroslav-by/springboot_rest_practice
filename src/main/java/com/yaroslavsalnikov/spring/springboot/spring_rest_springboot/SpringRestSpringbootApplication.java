e73317b0ce2cfdd06ad6518a1e5e44f704a8aded
Yaroslav <asgen-yaroslav@mail.ru>
2023-02-19 18:49:07 +0300

    Ready to work

---
+++ src/main/java/com/yaroslavsalnikov/spring/springboot/spring_rest_springboot/SpringRestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.yaroslavsalnikov.spring.springboot.spring_rest_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaroslavsalnikov/spring/springboot/spring_rest_springboot/SpringRestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaroslavsalnikov.spring.springboot.spring_rest_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
